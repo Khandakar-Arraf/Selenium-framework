3643d9855db95f020251423d157bae048be79054
Khandaker Arraf <52911060+Khandakar-Arraf@users.noreply.github.com>
2025-10-21 01:50:46 +0600

    Add files via upload
    adding files

---
+++ src/test/java/com/parabank/parasoft/test/FundTransfer.java
@@ -0,0 +1,54 @@
+package com.parabank.parasoft.test;
+
+import com.parabank.parasoft.page.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TransferFundsTest extends BaseTest {
+    @Test
+    public void transferFundsV1ShouldSucceed() {
+        Login_page loginPg = pg.goTo(Login_page.class);
+
+        OverViewPage overViewPg = loginPg
+                .fillUsername(getUsername())
+                .fillPassword(getPassword())
+                .clickLoginBtn();
+        Assert.assertTrue(overViewPg.isLogOutLinkDisplayed());
+
+        TransferFundsPage openNewAccountPg = overViewPg
+                .clickTransferFundsLink();
+
+        int amount = 1000;
+        TransferCompletePage transferCompletePg = openNewAccountPg
+                .fillAmount(amount)
+                .clickTransferButton();
+
+        Assert.assertTrue(transferCompletePg.isTransferComplete(amount));
+    }
+
+    @Test
+    public void transferFundsV2ShouldSucceed() {
+        int amount = 1000;
+        TransferCompletePage transferCompletePg = pg.goTo(Login_page.class)
+                .fillname(getUsername())
+                .fillPassword(getPassword())
+                .clickLoginBtn()
+                .clickTransferFundsLink()
+                .fillAmount(amount)
+                .clickTransferButton();
+        Assert.assertTrue(transferCompletePg.isTransferComplete(amount));
+    }
+
+
+    @Test
+    public void transferFundsV3ShouldSucceed() {
+        int amount = 1000;
+        TransferCompletePage transferCompletePg = pg.goTo(LoginPage.class)
+                .clickRegisterLink()
+                .doRegister()
+                .clickTransferFundsLink()
+                .fillAmount(amount)
+                .clickTransferButton();
+        Assert.assertTrue(transferCompletePg.isTransferComplete(amount));
+    }
+}
